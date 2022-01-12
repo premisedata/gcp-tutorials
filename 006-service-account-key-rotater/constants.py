39e42617aa9baa0222b805fe014112e1dacc1cdb
Qua Jones <90266631+qua-jones@users.noreply.github.com>
2022-01-12 10:55:34 -0500

    Service Account Key Rotater (#8)
    * service account key rotater
    
    * updated phrasing

---
+++ 006-service-account-key-rotater/constants.py
@@ -0,0 +1,11 @@
+DEFAULT_SECRET_NAME = "gcp_sa_key"
+
+SECRET_NAME_FIELD = "sn"
+SERVICE_ACCOUNT_NAME_FIELD = "service_account_name"
+PROJECT_FIELD = "project"
+TYPE_FIELD = "type"
+
+GH_REPO_FIELD = "ghr"
+GH_ORG_FIELD = "gho"
+
+LABEL_DELIMITER = "-_"
