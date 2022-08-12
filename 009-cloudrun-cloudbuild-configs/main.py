2230701993fb101b1116cd55ee2c21f484b70b7c
luke-xue <luke.xue@premise.com>
2022-08-12 14:20:56 -0600

    tweaks to goblet version and app

--- 009-cloudrun-cloudbuild-configs/main.py
+++ 009-cloudrun-cloudbuild-configs/main.py
@@ -3,6 +3,6 @@ from goblet import Goblet, goblet_entrypoint
 app = Goblet(function_name = "cloudrun-cloudbuild-configs", backend="cloudrun", routes_type="cloudrun")
 goblet_entrypoint(app)
 
-@app.route('/home')
-def home():
-    return {"hello": "world"}
+@app.route('/hello')
+def hello():
+    return "world"
